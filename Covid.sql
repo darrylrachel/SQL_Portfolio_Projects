cfa3c686ca6559205f362b3e093703848f51f05a
Darryl Rachel <racheldarryl@icloud.com>
2023-06-07 17:57:22 -0700

    Update Covid.sql

--- Covid.sql
+++ Covid.sql
@@ -2,7 +2,6 @@
 Covid 19 Data Exploration 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
--- Create Database PortfolioProjects
 
 Select *
   From CovidDeaths
